4c17eba999d72195d6d57cb6cf686308ed61e788
Varsha <45787710+Varsha-Chalasani@users.noreply.github.com>
2020-08-14 13:26:38 +0530

    edited class name

--- remover/RemoveOggy.java
+++ remover/RemoveOggy.java
@@ -3,7 +3,7 @@ package remover;
 import java.util.*;
 import java.util.stream.Collectors;
 
-public class Main {
+public class RemoveOggy {
     public static void removeOggy(ArrayList<String> names) {
         for(int i=names.size()-1; i>=0;i--){
             if(names.get(i).contains("oggy")){
