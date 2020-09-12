6c3f59c9f28b676c6bd00ce55f53addabe0ec46a
Varsha <45787710+Varsha-Chalasani@users.noreply.github.com>
2020-09-12 20:31:46 +0530

    Update RemoveOggy.java

--- remover/RemoveOggy.java
+++ remover/RemoveOggy.java
@@ -4,27 +4,9 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class RemoveOggy {
-    public static void removeOggy(ArrayList<String> names) {
-        for(int i=names.size()-1; i>=0;i--){
-            if(names.get(i).contains("oggy")){
-                names.remove(i);
-            }
-        }
-        return;
-    }
-    public static boolean oggyIsRemoved(ArrayList<String> names) {
-        List<String> oggys = names.stream().filter(it -> it.contains("oggy")).collect(Collectors.toList());
-        return oggys.size() == 0;
-    }
+    
     public static void main(String[] args) {
-        ArrayList<String> names = new ArrayList<String>(Arrays.asList("nobita", "bheem", "oggy", "oggy cockroach", "ninja"));
-        removeOggy(names);
-        if(oggyIsRemoved(names)) {
-            System.out.println("Passed!");
-            System.exit(0);
-        } else {
-            System.out.println("Failed!!");
-            System.exit(1);
-        }
+       string path = System.getProperty("user.dir");
+       System.out.println(path);
     }
 }
