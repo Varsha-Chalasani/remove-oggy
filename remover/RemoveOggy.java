f90f95ee5a669426e571367a8f06217846da30ba
Varsha <45787710+Varsha-Chalasani@users.noreply.github.com>
2020-09-12 20:33:40 +0530

    Update RemoveOggy.java

--- remover/RemoveOggy.java
+++ remover/RemoveOggy.java
@@ -6,7 +6,6 @@ import java.util.stream.Collectors;
 public class RemoveOggy {
     
     public static void main(String[] args) {
-       string path = System.getProperty("user.dir");
-       System.out.println(path);
+       System.out.println(System.getProperty("user.dir"););
     }
 }
